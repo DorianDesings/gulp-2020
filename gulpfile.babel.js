380e7379b831cbce7c8c4734efa228e8cbb3269d
DorianDesings <dorianoriginaldesings@gmail.com>
2020-05-29 00:48:21 +0200

    gulp + pug + sass + purgeCss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,8 +11,19 @@ import gulp from 'gulp'
 import babel from 'gulp-babel'
 import terser from 'gulp-terser'
 
+//PUG
+import pug from 'gulp-pug'
+
+//SASS
+import sass from 'gulp-sass'
+
 //Common
 import concat from 'gulp-concat'
+
+//Clean CSS
+import clean from 'gulp-purgecss'
+
+const production = true
 
 //Variables/constantes
 const cssPlugins = [
@@ -47,8 +58,35 @@ gulp.task('babel', () => {
         .pipe(gulp.dest('./public/js'))
 })
 
+gulp.task('views', () => {
+    return gulp.src('./src/views/pages/*.pug')
+        .pipe(pug({
+            pretty: production ? false : true
+        }))
+        .pipe(gulp.dest('./public'))
+})
+
+gulp.task('sass', () => {
+    return gulp.src('./src/scss/styles.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(postcss(cssPlugins))
+        .pipe(gulp.dest('./public/css'))
+})
+
+gulp.task('clean', () => {
+    return gulp.src('./public/css/styles.css')
+        .pipe(clean({
+            content: ['./public/*.html']
+        }))
+        .pipe(gulp.dest('./public/css'))
+})
+
 gulp.task('default', () => {
-    gulp.watch('./src/*.html', gulp.series('html-min'))
-    gulp.watch('./src/css/*.css', gulp.series('styles'))
+    // gulp.watch('./src/*.html', gulp.series('html-min'))
+    // gulp.watch('./src/css/*.css', gulp.series('styles'))
+    gulp.watch('./src/views/**/*.pug', gulp.series('views'))
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'))
     gulp.watch('./src/js/*.js', gulp.series('babel'))
 })
