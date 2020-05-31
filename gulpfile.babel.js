ec10e16964e2c06abb274ac11cddecb9b30a5131
DorianDesings <dorianoriginaldesings@gmail.com>
2020-05-31 11:14:10 +0200

    Gulp + cache bust + imagemin + browser sync + plumber

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,7 +23,19 @@ import concat from 'gulp-concat'
 //Clean CSS
 import clean from 'gulp-purgecss'
 
-const production = true
+//Caché bust
+import cacheBust from 'gulp-cache-bust'
+
+//Optimización imágenes
+import imagemin from 'gulp-imagemin'
+
+//Browser sync
+import { init as server, stream, reload } from 'browser-sync'
+
+//Plumber
+import plumber from 'gulp-plumber'
+
+const production = false
 
 //Variables/constantes
 const cssPlugins = [
@@ -34,6 +46,7 @@ const cssPlugins = [
 gulp.task('html-min', () => {
     return gulp
         .src('./src/*.html')
+        .pipe(plumber())
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true
@@ -44,14 +57,17 @@ gulp.task('html-min', () => {
 gulp.task('styles', () => {
     return gulp
         .src('./src/css/*.css')
+        .pipe(plumber())
         .pipe(concat('styles-min.css'))
         .pipe(postcss(cssPlugins))
         .pipe(gulp.dest('./public/css'))
+        .pipe(stream())
 })
 
 gulp.task('babel', () => {
     return gulp
         .src('./src/js/*.js')
+        .pipe(plumber())
         .pipe(concat('scripts-min.js'))
         .pipe(babel())
         .pipe(terser())
@@ -60,33 +76,54 @@ gulp.task('babel', () => {
 
 gulp.task('views', () => {
     return gulp.src('./src/views/pages/*.pug')
+        .pipe(plumber())
         .pipe(pug({
             pretty: production ? false : true
+        }))
+        .pipe(cacheBust({
+            type: 'timestamp'
         }))
         .pipe(gulp.dest('./public'))
 })
 
 gulp.task('sass', () => {
     return gulp.src('./src/scss/styles.scss')
+        .pipe(plumber())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(postcss(cssPlugins))
         .pipe(gulp.dest('./public/css'))
+        .pipe(stream())
 })
 
 gulp.task('clean', () => {
     return gulp.src('./public/css/styles.css')
+        .pipe(plumber())
         .pipe(clean({
             content: ['./public/*.html']
         }))
         .pipe(gulp.dest('./public/css'))
 })
 
+gulp.task('imgmin', () => {
+    return gulp.src('./src/images/*')
+        .pipe(plumber())
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 30, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 1 })
+        ]))
+        .pipe(gulp.dest('./public/images'))
+})
+
 gulp.task('default', () => {
-    // gulp.watch('./src/*.html', gulp.series('html-min'))
+    server({
+        server: './public'
+    })
+    // gulp.watch('./src/*.html', gulp.series('html-min')).on('change', reload)
     // gulp.watch('./src/css/*.css', gulp.series('styles'))
-    gulp.watch('./src/views/**/*.pug', gulp.series('views'))
+    gulp.watch('./src/views/**/*.pug', gulp.series('views')).on('change', reload)
     gulp.watch('./src/scss/**/*.scss', gulp.series('sass'))
-    gulp.watch('./src/js/*.js', gulp.series('babel'))
+    gulp.watch('./src/js/*.js', gulp.series('babel')).on('change', reload)
 })
